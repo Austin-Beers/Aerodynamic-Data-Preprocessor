7a06616dda633d246fa090c0fd9e9eb9754f3d81
Austin Beers <beersa6@students.rowan.edu>
2021-01-24 23:33:00 -0500

    Fixed comments and data discrepency

--- Preprocess.py
+++ Preprocess.py
@@ -7,22 +7,17 @@ class PreprocessData:
     dir_name = ''
     df_ = pd.DataFrame()
     df_ = df_.fillna(0)
-    
 
     def __init__(self):
-        self.dir_name = '/Users/TheDongBringer/Desktop/ProjectBoom/Test'
+        self.dir_name = 'insert/path/todirectory/here'
         self.processDir()
         display(self.df_)
 
     def toDf(self, csv):
-        
         temp_df = pd.read_csv(csv, index_col=False)
-        
-        #new_df = temp_df.tail(1)
         new_df = temp_df.iloc[-1]
         print(new_df)
         self.df_ = self.df_.append(new_df, ignore_index=True)
-
 
     def processDir(self):
         for filename in os.listdir(self.dir_name):
@@ -30,12 +25,6 @@ class PreprocessData:
             print(temp_str)
             self.toDf(temp_str)
 
-            # with open(filename, 'rU') as f:
-            #     t = f.read()
-            #     toDf(t)
-            #     t = t.split()
-            #     print(t)
-
 if __name__ == '__main__':
     run_script = PreprocessData()
-    # print(run_script.df_)
+   
