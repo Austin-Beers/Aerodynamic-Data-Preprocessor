5c79e50095f9d607d6b225159fd4b59ab9d614ff
Austin Beers <beersa6@students.rowan.edu>
2021-01-24 23:30:51 -0500

    mvp

--- Preprocess.py
+++ Preprocess.py
@@ -1,25 +1,41 @@
 import os 
 import sys
 import pandas as pd 
+from IPython.display import display 
 
 class PreprocessData:
     dir_name = ''
+    df_ = pd.DataFrame()
+    df_ = df_.fillna(0)
+    
 
     def __init__(self):
-        self.dir_name = '/Users/abeers/Desktop/ProjectBoom/Test'
+        self.dir_name = '/Users/TheDongBringer/Desktop/ProjectBoom/Test'
         self.processDir()
+        display(self.df_)
+
+    def toDf(self, csv):
+        
+        temp_df = pd.read_csv(csv, index_col=False)
+        
+        #new_df = temp_df.tail(1)
+        new_df = temp_df.iloc[-1]
+        print(new_df)
+        self.df_ = self.df_.append(new_df, ignore_index=True)
+
 
     def processDir(self):
         for filename in os.listdir(self.dir_name):
-            with open(filename, 'rU') as f:
-                t = f.read()
-                t = t.split()
-                print(t)
+            temp_str = self.dir_name + "/" + filename
+            print(temp_str)
+            self.toDf(temp_str)
 
-    # def toDf(self, df):
-
-
-
+            # with open(filename, 'rU') as f:
+            #     t = f.read()
+            #     toDf(t)
+            #     t = t.split()
+            #     print(t)
 
 if __name__ == '__main__':
     run_script = PreprocessData()
+    # print(run_script.df_)
